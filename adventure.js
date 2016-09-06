0d2b7806f9494a4acff97fe2d2f3bb1bf2a26f48
Adam Pendery <adampendery@gmail.com>
2016-09-06 04:57:50 -0500

    inner scroll img

--- adventure.js
+++ adventure.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
   $("#flipbook").turn({
-    width: 960,
+    width: 985,
     height: 360,
   });
 });
+
+function ScrollDiv(){
+
+ if(document.getElementById('ecran').scrollTop<(document.getElementById('ecran').scrollHeight-document.getElementById('ecran').offsetHeight)){-1
+    document.getElementById('ecran').scrollTop=document.getElementById('ecran').scrollTop+1
+		}
+	}
+
+setInterval(ScrollDiv,50)
